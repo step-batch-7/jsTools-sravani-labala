4cd43f4667d56350cbf9ad2dbb21a74a03bbd0b8
Sravani Labala <sravani.labala@gmail.com>
2019-12-26 15:03:32 +0530

    modified parseOptions function in the tailLib.js

--- src/tailLib.js
+++ src/tailLib.js
@@ -3,24 +3,22 @@ const extractLines = function(numberOfLines, content) {
   return splitContent.slice(-numberOfLines).join("\n");
 };
 
-const parseOptions = function(commandLineArgs) {
-  let parsedArgs = { lines: 10, fileName: commandLineArgs[0] };
-  const isCountValid = Number.isInteger(+commandLineArgs[1]);
-  const isOptionValid = commandLineArgs[0] == "-n";
-  const inputError = `tail: illegal offset -- ${commandLineArgs[1]}`;
-  if (isOptionValid && !isCountValid) return { inputError };
-  if (isOptionValid) {
-    parsedArgs.lines = Math.abs(+commandLineArgs[1]);
-    parsedArgs.fileName = commandLineArgs[2];
-  }
-  return { parsedArgs, inputError: "" };
-};
-
 const loadContent = function(fileName, fs, encoding) {
   const { existsSync, readFileSync } = fs;
   const error = `tail: ${fileName}: no such file or directory`;
   if (!existsSync(fileName)) return { error, content: "" };
   return { error: "", content: readFileSync(fileName, encoding) };
+};
+
+const parseOptions = function(commandLineArgs) {
+  let parsedArgs = { lines: 10, fileName: commandLineArgs[0] };
+  const inputError = `tail: illegal offset -- ${commandLineArgs[1]}`;
+  if (commandLineArgs[0] == "-n") {
+    if (!Number.isInteger(+commandLineArgs[1])) return { inputError };
+    parsedArgs.lines = Math.abs(+commandLineArgs[1]);
+    parsedArgs.fileName = commandLineArgs[2];
+  }
+  return { parsedArgs, inputError: "" };
 };
 
 const tail = function(commandLineArgs, fs, encoding) {
